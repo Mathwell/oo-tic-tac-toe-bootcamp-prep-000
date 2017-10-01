b27f4966050490be6477370c3c30faba29354063
Olena Ageyeva <eageeva@hotmail.com>
2017-10-01 20:38:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -10,24 +10,82 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
+#*********************************************
   def input_to_index(input)
    input.to_i - 1
   end
-
-  def current_player
-    turn_count % 2 == 0 ? "X" : "O"
+#********************************************
+def move(index, current_player = "X")
+  @board[index] = current_player
+end
+#*********************************************
+def position_taken?(index)
+  @board[index] != " " && @board[index] != ""
+end
+#*********************************************
+def valid_move?(index)
+  index.between?(0,8) && !position_taken?(index)
+end
+#********************************************  
+def turn
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+  if valid_move?(index)
+    move(index)
+    display_board
+  else
+    turn
   end
-
-  def turn_count
-    @board.count{|token| token == "X" || token == "O"}
+end
+#*********************************************
+def turn_count
+  @board.count{|token| token == "X" || token == "O"}
+end
+#*********************************************
+def current_player
+  turn_count % 2 == 0 ? "X" : "O"
+end
+#*********************************************
+def won?
+  WIN_COMBINATIONS.detect { |combination|
+    board[combination[0]]==board[combination[1]] && board[combination[1]]==board[combination[2]] && board[combination[1]]!=" "
+  }
+end
+#*********************************************
+def full?
+    @board.count {|cell|   cell=="X" || cell=="O"}==9
+end
+#*********************************************
+def draw?
+  full? && !won?
+    
+end
+#*****************************
+def over?
+ full? || won? || draw?
+end
+#*********************************************
+def winner
+if !won?
+  return nil
+end
+return @board[won?[0]]
+end
+#********************************************
+def play
+  puts "Welcome to Tic Tac Toe!"
+  display_board
+  while !over? do
+    turn
   end
-
-  def display_board
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  if won?
+    puts "#{winner(board)} wins!"
   end
+  if draw?
+    puts "Game over! Noone wins."
+  end
+end  
+
+  
 end
