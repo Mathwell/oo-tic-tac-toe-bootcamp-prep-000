2607781ccee72c69fcd34a23c1df9671c2eb3620
Olena Ageyeva <eageeva@hotmail.com>
2017-10-01 20:51:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,7 +32,7 @@ def turn
   puts "Please enter 1-9:"
   input = gets
   index = input_to_index(input)
-  if valid_move?(index)
+  if valid_move?(index, current_player)
     move(index)
     display_board
   else
