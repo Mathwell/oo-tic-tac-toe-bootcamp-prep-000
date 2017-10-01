daa75490cd5b3e338390290806238c2988d89fbb
Olena Ageyeva <eageeva@hotmail.com>
2017-10-01 20:41:12 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,6 +1,7 @@
 class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
+    @WIN_COMBINATIONS=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   end
 
   def display_board
@@ -48,8 +49,8 @@ def current_player
 end
 #*********************************************
 def won?
-  WIN_COMBINATIONS.detect { |combination|
-    board[combination[0]]==board[combination[1]] && board[combination[1]]==board[combination[2]] && board[combination[1]]!=" "
+  @WIN_COMBINATIONS.detect { |combination|
+    @board[combination[0]]==@board[combination[1]] && @board[combination[1]]==@board[combination[2]] && @board[combination[1]]!=" "
   }
 end
 #*********************************************
